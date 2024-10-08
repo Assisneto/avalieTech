8aaabf39d015ab0aa798dfb9e470ef5d3a1553c1
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-10-08 20:13:18 -0300

    add sucess modal after send email

--- lib/avalie_tech_web/live/landing_live.ex
+++ lib/avalie_tech_web/live/landing_live.ex
@@ -1,16 +1,16 @@
 defmodule AvalieTechWeb.LandingLive do
+  use AvalieTechWeb, :live_view
   alias AvalieTech.Mailer
   alias AvalieTech.Users.UserEmail
-  use AvalieTechWeb, :live_view
   require Logger
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, form_submitted: false)}
   end
 
   @impl true
-  def(handle_event("try_now", %{"email" => email, "name" => name}, socket)) do
+  def handle_event("try_now", %{"email" => email, "name" => name}, socket) do
     user = %{name: name, email: email}
 
     email = UserEmail.experiment_email(user)
@@ -18,11 +18,15 @@ defmodule AvalieTechWeb.LandingLive do
     case Mailer.deliver(email) do
       {:ok, _metadata} ->
         Logger.info("E-mail enviado com sucesso para #{user.email}")
-        {:noreply, socket}
+        {:noreply, assign(socket, form_submitted: true)}
 
       {:error, reason} ->
         Logger.error("Falha ao enviar e-mail: #{inspect(reason)}")
         {:noreply, socket}
     end
+  end
+
+  def handle_event("close_thank_you", _, socket) do
+    {:noreply, assign(socket, form_submitted: false)}
   end
 end
