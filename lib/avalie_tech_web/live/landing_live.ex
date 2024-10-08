8075e9585f4a78c3d8952160b1841c8a1de4299a
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-10-08 17:30:24 -0300

    add modal to get infos and send email

--- lib/avalie_tech_web/live/landing_live.ex
+++ lib/avalie_tech_web/live/landing_live.ex
@@ -1,8 +1,21 @@
 defmodule AvalieTechWeb.LandingLive do
+  alias AvalieTech.Mailer
+  alias AvalieTech.Users.UserEmail
   use AvalieTechWeb, :live_view
 
   @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("try_now", %{"email" => email, "name" => name}, socket) do
+    user = %{name: name, email: email}
+
+    user
+    |> UserEmail.experiment_email()
+    |> Mailer.deliver()
+
+    {:noreply, socket}
   end
 end
