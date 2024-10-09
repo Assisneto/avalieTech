de90dfe06bee6b0a19262b99a7cd5a5ff647cdb6
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-10-09 18:12:35 -0300

    add template email lib

--- mix.exs
+++ mix.exs
@@ -61,7 +61,8 @@ defmodule AvalieTech.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:gen_smtp, "~> 1.2"}
+      {:gen_smtp, "~> 1.2"},
+      {:phoenix_swoosh, "~> 1.2.1"}
     ]
   end
 
