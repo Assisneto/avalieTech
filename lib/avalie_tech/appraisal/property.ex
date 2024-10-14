2f4e6eef53cc386e40e43e18a48754b9a20dfe33
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-10-14 20:14:11 -0300

    add cast_assoc in property to create appraisal together property

--- lib/avalie_tech/appraisal/property.ex
+++ lib/avalie_tech/appraisal/property.ex
@@ -2,7 +2,7 @@ defmodule AvalieTech.Appraisal.Property do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias AvalieTech.Appraisal.Address
+  alias AvalieTech.Appraisal.{Address, AppraisalReport}
 
   @primary_key {:id, Ecto.UUID, autogenerate: true}
   schema "properties" do
@@ -10,6 +10,7 @@ defmodule AvalieTech.Appraisal.Property do
     field :registration_number, :string
 
     has_one :address, Address, on_delete: :delete_all
+    has_many :appraisal_reports, AppraisalReport, on_delete: :delete_all
 
     timestamps(type: :utc_datetime)
   end
@@ -20,5 +21,6 @@ defmodule AvalieTech.Appraisal.Property do
     |> cast(attrs, [:owner, :registration_number])
     |> validate_required([:owner, :registration_number])
     |> cast_assoc(:address, required: true)
+    |> cast_assoc(:appraisal_reports, required: true, with: &AppraisalReport.changeset/2)
   end
 end
